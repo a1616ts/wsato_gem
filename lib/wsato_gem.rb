9d42933e193c58e245b145440a239efeed8df703
Naoko Sato <a1617ns@aiit.ac.jp>
2016-08-06 17:51:08 +0900

    first commit - wsato_gem.rb

--- lib/wsato_gem.rb
+++ lib/wsato_gem.rb
@@ -1,5 +1,28 @@
 require "wsato_gem/version"
+require 'open-uri'
 
 module WsatoGem
-  # Your code goes here...
+	def input()
+		input = raw_input()
+	  if input.include?("exit")
+	  	exit(0)
+	  return input
+	end
+	
+	def output(message):
+		printf("library > %s", message)
+	end
+	
+	def output_br():
+		printf("library >")
+	end
+	
+	def get_request(url)
+		results = open(url)
+		code, message = results.status # res.status => ["200", "OK"]
+		if code != 200
+			raise "HTTP error!" # => RuntimeError
+		else:
+			return results
+	end
 end

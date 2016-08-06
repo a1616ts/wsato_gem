01bf6bcaddecaa39b9a3b8b2cb675192d11ef495
Naoko Sato <a1617ns@aiit.ac.jp>
2016-08-06 19:23:19 +0900

    adding review changes5

--- lib/wsato_gem/library.rb
+++ lib/wsato_gem/library.rb
@@ -1,27 +1,27 @@
 require 'open-uri'
 
 def input()
-	input = STDIN.gets() # statndard input
-  if input.include?("exit")
-  	exit(0)
-	end
-  return input
+    input = STDIN.gets() 
+    if input.include?("exit")
+        exit(0)
+    end
+    return input
 end
 
 def output(message)
-	printf("library > %s", message)
+    printf("library > %s", message)
 end
 
 def output_br()
-	printf("library >")
+    printf("library >")
 end
 
 def get_request(url)
-	results = open(url)
-	code, message = results.status # res.status => ["200", "OK"]
-	if code != 200
-		raise "HTTP error!" # => RuntimeError
-	else
-		return results
-	end
+    results = open(url)
+    code, message = results.status # res.status => ["200", "OK"]
+    if code != 200
+        raise "HTTP error!" # => RuntimeError
+    else
+        return results
+    end
 end

79fabb3998f27f5d48cd9df399ec1aa7fb84907c
a1616ts <a1616ts@aiit.ac.jp>
2016-08-11 01:38:05 +0900

    Adjustment.

--- lib/wsato_gem.rb
+++ lib/wsato_gem.rb
@@ -1,7 +1,8 @@
-require "wsato_gem/version"
+require_relative 'wsato_gem/version'
+require_relative 'wsato_gem/library'
 
 module WsatoGem
-  def search_library(address:, calilapp_key:)
-    return Library.new.search(address:, calilapp_key:)
+  def self.search_library(address:, calilapp_key:)
+    return Library.new.search(address: address, calilapp_key: calilapp_key)
   end
 end
--- lib/wsato_gem/library.rb
+++ lib/wsato_gem/library.rb
@@ -10,41 +10,40 @@ def input()
 end
 
 def output(message)
-    printf("library > %s", message)
+    printf("library > %s\n", message)
 end
 
 def output_br()
-    printf("library >")
+    printf("library >\n")
 end
 
 def get_request(url)
     results = open(url)
     code, message = results.status # res.status => ["200", "OK"]
-    if code != 200
+    if code != '200'
         raise "HTTP error!" # => RuntimeError
     else
-        return results
+        return results.read
     end
 end
 
 class Library
-  def self.search(address:, calilapp_key:, libraries_limit:)
+  def search(address:, calilapp_key:, libraries_limit: 30)
     begin
       results = request_to_geocode_api(address: address)
-      content = json.loads(results.content)
-      location = content['results'][0]['geometry']['location']
+      contents = JSON.load(results)
+      location = contents['results'][0]['geometry']['location']
       geocode = location['lng'].to_s << ',' << location['lat'].to_s
 
       results = request_to_calil_api(calilapp_key: calilapp_key, geocode: geocode, libraries_limit: libraries_limit)
-      contents = JSON.parse(results.content)
-
-      if contents.legth < 1
+      contents = JSON.parse(results)
+      if contents.length < 1
         output('お近くに図書館がないようです')
         raise
       else
         output('お近くの図書館を検索しました')
       end
-      return libraries_in_neihborhood(content)
+      return libraries_in_neihborhood(contents)
     rescue
       raise
     end
